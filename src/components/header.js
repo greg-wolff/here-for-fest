1fdfd8adb55b33c8e3306827ac55fc8df4986050
greg-wolff <gregwwolff@gmail.com>
2022-10-18 23:21:44 -0700

    updated ogimage and hopefully fixed emoji mobile bug

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,8 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { useBreakpoint } from 'gatsby-plugin-breakpoints';
 
-
-
-const Weather = () => {
+const Header = ({ siteTitle }) => {
+  const breakpoints = useBreakpoint();
   const { data, isLoading, errorMessage } = useOpenWeather({
     key: 'e7f307596df41bb86a93f6b56f604889',
     lat: '34.0522',
@@ -15,22 +14,15 @@ const Weather = () => {
     lang: 'en',
     unit: 'imperial', // values are (metric, standard, imperial)
   });
-  return (
-    
-   <div>{isLoading ? `––°F ––%` : `${data && data.current.temperature.current}°F ${data && data.current.humidity}%`}</div>
-  )
-}
-
-const Header = ({ siteTitle }) => {
-  const breakpoints = useBreakpoint();
 
   return (
     <header>
+      { !isLoading && <>
       <div>Sat Dec 3</div>
       {breakpoints.sm ? null : <div class="tickets-button"><a href="https://fanimal.com/fanimal-event/here-for-la-fest-364403780249708256" target="_blank" rel="noreferrer">Get Tickets</a></div>}
       <div className="countdown">{moment("2022-12-04").diff(moment(), 'days') == 0 ? `TODAY` : `${moment("2022-12-04").diff(moment(), 'days')} DAYS UNTIL${breakpoints.sm ? `\n` : ` `}HERE FOR LA`}</div>
-      <div>{breakpoints.sm ? <div class="tickets-button"><a href="https://fanimal.com/fanimal-event/here-for-la-fest-364403780249708256" target="_blank" rel="noreferrer">Get Tickets</a></div> : <Weather />}</div>
-      {breakpoints.sm ? null : <div>Los Angeles, CA</div>}
+      <div>{breakpoints.sm ? <div class="tickets-button"><a href="https://fanimal.com/fanimal-event/here-for-la-fest-364403780249708256" target="_blank" rel="noreferrer">Get Tickets</a></div> : <div>{isLoading ? `––°F ––%` : `${data && data.current.temperature.current}°F ${data && data.current.humidity}%`}</div>}</div>
+      {breakpoints.sm ? null : <div>Los Angeles, CA</div>}</>}
     </header>
   )
 }
